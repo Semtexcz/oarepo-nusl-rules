57d683ba6875a63ff325dc6d54496fe18b0c6f87
Daniel Kopecky <kopecky.d@gmail.com>
2020-01-16 11:02:11 +0100

    Refactor register

--- oarepo_nusl_rules/register.py
+++ oarepo_nusl_rules/register.py
@@ -23,7 +23,7 @@ class Singleton:
 @Singleton
 class RuleRegistry(object):
 
-    def __init__(self, entry_point_group="oarepo_nusl_rules.rules"):  # entry_point_group="oarepo_nusl_rules.rules"
+    def __init__(self, entry_point_group="oarepo_nusl_rules.rules"):
         self.loaded = False
         self.rules = {}
         self.entry_point_group = entry_point_group
@@ -31,19 +31,7 @@ class RuleRegistry(object):
     def load(self):
         if not self.loaded:
             for entry_point in pkg_resources.iter_entry_points("oarepo_nusl_rules.rules"):
-                ep = entry_point.load()
-                pass
+                entry_point.load()
 
     def register(self, func):
         self.rules[func.__name__] = func
-        print(RuleRegistry.Instance(), RuleRegistry.Instance().rules)
-
-
-# RuleRegistry.Instance()
-
-if __name__ == "__main__":
-    c1 = RuleRegistry.Instance()
-    RuleRegistry.Instance().load()
-    print(RuleRegistry.Instance().rules, RuleRegistry.Instance())
-    c2 = RuleRegistry.Instance()
-    print(c1, c2, c1 is c2)
