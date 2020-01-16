615c00ae8b4cefa6027118cf383ee655aaa57570
Daniel Kopecky <kopecky.d@gmail.com>
2020-01-16 10:13:05 +0100

    Delete bad test

--- tests/test_register.py
+++ tests/test_register.py
@@ -7,9 +7,3 @@ def test_register_load():
     instance2 = RuleRegistry.Instance()
     assert len(RuleRegistry.Instance().rules) > 0
     assert instance1 is instance2
-
-
-def test_register_load_2():
-    inst1 = RuleRegistry(entry_point_group="bla")
-    inst2 = RuleRegistry()
-    assert inst1 is inst2
