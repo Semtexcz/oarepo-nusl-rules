3f0a1ab3508e6c0271a6d5318d18f35758ab75ff
Daniel Kopecky <kopecky.d@gmail.com>
2020-01-17 11:04:01 +0100

    Added test
    ...that check if the rule_registry is the same instance as RuleRegistry.Instance()

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,3 +1,4 @@
+from oarepo_nusl_rules import rule_registry
 from oarepo_nusl_rules.register import RuleRegistry
 
 
@@ -6,4 +7,10 @@ def test_register_load():
     RuleRegistry.Instance().load()
     instance2 = RuleRegistry.Instance()
     assert len(RuleRegistry.Instance().rules) > 0
+    assert instance1 is instance2
+
+
+def test_register_instance():
+    instance1 = rule_registry
+    instance2 = RuleRegistry.Instance()
     assert instance1 is instance2
